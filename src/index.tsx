d455263b559f53aee06189ef31ed68556574f10b
maxgurewitz <maxthegeek1@gmail.com>
2016-08-09 02:24:29 -0700

    basic redux integration

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,34 @@ import {render as reactRender} from 'react-dom';
 import {createStore} from 'redux';
 import {connect, Provider} from 'react-redux';
 
+function view(state : Model) {
+  return (<div>count: {state.count}</div>);
+}
+
+interface Model {
+  count: Number;
+}
+
+interface Action {
+  type: String;
+}
+
+const PersonalSite = connect((state : Model) => state)(view);
+
+function update(state : Model, action: Action) {
+  return { count: 1 };
+}
+
 export default {
   render(selector: string) {
-    reactRender(<div>foo</div>, document.querySelector(selector));
+    const store = createStore(update);
+
+    const app = (
+      <Provider store = {store}>
+        <PersonalSite />
+      </Provider>
+    );
+
+    reactRender(app, document.querySelector(selector));
   }
 };
