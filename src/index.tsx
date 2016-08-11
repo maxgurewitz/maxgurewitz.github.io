23af99c61957bc18b7cc54d90b2fd6a7fd9225b8
maxgurewitz <maxthegeek1@gmail.com>
2016-08-11 09:42:15 -0700

    wip

--- src/index.tsx
+++ src/index.tsx
@@ -11,6 +11,11 @@ enum ActionType {
   SwitchPage
 }
 
+enum Page {
+  About,
+  Resume
+}
+
 function increment(dispatch : Dispatch) {
   return () => dispatch({ type: ActionType.Increment });
 }
@@ -28,13 +33,15 @@ interface View {
   (payload : ViewPayload) : JSX.Element;
 }
 
+const navBarHeight = 3;
+
 const navBarStyle = {
   alignItems: 'center',
   borderRadius: '2px',
   boxShadow: '0 8px 17px 0 rgba(0,0,0,0.2),0 6px 20px 0 rgba(0,0,0,0.19)',
   color: 'white',
   display: 'flex',
-  height: '3em',
+  height: `${navBarHeight}em`,
   justifyContent: 'flex-end',
   left: 0,
   margin: 0,
@@ -62,16 +69,45 @@ const avatarStyle = {
   maxWidth: '80%'
 };
 
+const contentStyle = {
+  position: 'absolute',
+  top: `${navBarHeight * 1.5}em`
+};
+
+const aboutView : View = function aboutView(payload) {
+  return (
+    <div>
+      about
+    </div>
+  );
+}
+
+const resumeView : View = function resumeView(payload) {
+  return (
+    <div>
+      resume
+    </div>
+  );
+}
+
+const PageViewCases : Cases<View> = {
+  [Page.About]: aboutView,
+  [Page.Resume]: resumeView,
+  default: aboutView
+};
+
 const view : View = function view(payload) {
   const {state, dispatch} = payload;
 
+  console.log('loc2', state);
+  // FIXME: dispatch isn't working
   return (
     <div>
       <div style={navBarStyle}>
         <img src={HEADSHOT} style={avatarStyle}/>
 
         <div style={navItemStyle}>
-          <a style={linkBlockStyle} onClick={switchPage(Page.About, dispatch)}> about </a>
+          <div style={linkBlockStyle} onClick={switchPage(Page.About, dispatch)}> about </div>
         </div>
 
         <div style={navItemStyle}>
@@ -84,13 +120,11 @@ const view : View = function view(payload) {
           </a>
         </div>
       </div>
+      <div style={contentStyle}>
+        {evaluateCase(state.page, PageViewCases)(payload)}
+      </div>
     </div>
   );
-}
-
-enum Page {
-  About,
-  Resume
 }
 
 interface Model {
@@ -111,9 +145,9 @@ interface Update {
   (state : Model, action : Action) : Model;
 }
 
-interface Cases {
-  [index : number] : Update,
-  default : Update
+interface Cases<T> {
+  [index : number] : T,
+  default : T
 }
 
 const INITIAL_STATE : Model = {
@@ -123,7 +157,7 @@ const INITIAL_STATE : Model = {
 
 const PersonalSite = connect((state : Model) => ({state}), (dispatch : Dispatch) => ({dispatch}))(view);
 
-const cases : Cases = {
+const updateCases : Cases<Update> = {
   [ActionType.Increment]: (state : Model) => assign(state, { count: state.count + 1 }),
 
   [ActionType.SwitchPage]: (state : Model, action : Action) =>
@@ -132,10 +166,13 @@ const cases : Cases = {
   default: (state : Model) => state
 };
 
-function update(state : Model, action: Action) {
-  const updateCase : Update = cases[action.type] || cases.default;
+function evaluateCase<T>(type : number, cases : Cases<T>) : T {
+  return cases[type] || cases.default;
+}
 
-  return updateCase(state, action);
+function update(state : Model, action: Action) {
+  console.log('loc1', action);
+  return evaluateCase(action.type, updateCases)(state, action);
 }
 
 export default {
