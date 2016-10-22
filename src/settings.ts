37fcf9915edbd31f44dc68d56220310034e255fc
Max Gurewitz <maxthegeek1@gmail.com>
2016-10-22 03:02:39 +0300

    nested views being rendered

--- src/settings.ts
+++ src/settings.ts
@@ -1,2 +1,4 @@
 export const navBarHeight = 3;
+export const nestingFactor = 2;
 export const maxViewDepth = 10;
+export const baseFontSize = 16;
--- src/views/analytics.tsx
+++ src/views/analytics.tsx
@@ -1,16 +1,48 @@
+import {identity} from 'lodash';
 import * as t from '../types';
 import * as React from 'react';
 import main from './main';
+import {baseFontSize, nestingFactor} from '../settings';
 
-const nestingFactor = 2;
 const sizePercentage = (100/nestingFactor)+'%';
 
 const analyticsView : t.View = function analyticsView(payload) {
-  const {config} = payload;
+  const {state, config} = payload;
+
+  const nestedViewStyle = {
+    position: 'relative',
+    width: sizePercentage,
+    height: sizePercentage,
+    margin: '0 auto',
+    zIndex: 5,
+    border: '1px solid #ddd',
+    fontSize: baseFontSize/Math.pow(nestingFactor, config.viewIndex + 1) + 'px',
+  };
+
+  const controlsContainerStyle = {
+    width: sizePercentage,
+    height: '2em',
+    borderStyle: 'solid',
+    borderColor: '#ddd',
+    margin: '0 auto',
+    borderWidth: '0 1px 1px 1px'
+  };
+
+  const nestedView = (config.viewIndex + 1) < state.views.length ?
+    main({
+      config: { viewIndex: config.viewIndex + 1 },
+      dispatch: identity,
+      state
+    }) : "";
 
   return (
     <div>
-      analytics content
+      <div style={nestedViewStyle} >
+        {nestedView}
+      </div>
+      <div style={controlsContainerStyle}>
+        play
+      </div>
     </div>
   );
 }
