7cc3b33964bbfcfdd8c926f7fc2683e74df69330
maxgurewitz <maxgurewitz@gmail.com>
2020-03-28 19:11:46 -0700

    full elm program with elm ui

--- src/Main.elm
+++ src/Main.elm
@@ -1,9 +1,71 @@
 module Main exposing (main)
 
-import Element exposing (text)
-import Html exposing (Html)
+import Browser exposing (Document)
+import Element exposing (Element, column, fill, padding, row, spacing, text, width)
 
 
-main : Html msg
+type Msg
+    = NoOp
+
+
+type BlogEntry
+    = Mit6824
+
+
+type alias Model =
+    { blogEntry : BlogEntry
+    }
+
+
+view : Model -> Document Msg
+view _ =
+    { title = "Max Gurewitz"
+    , body =
+        [ Element.layout []
+            (column [ width fill ]
+                [ header
+                , content
+                ]
+            )
+        ]
+    }
+
+
+header : Element Msg
+header =
+    row [ width fill, padding 20, spacing 20 ] [ text "header" ]
+
+
+content : Element Msg
+content =
+    row [ width fill ] [ text "content" ]
+
+
+init : () -> ( Model, Cmd Msg )
+init _ =
+    let
+        model =
+            { blogEntry = Mit6824
+            }
+    in
+    ( model, Cmd.none )
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update _ model =
+    ( model, Cmd.none )
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Sub.none
+
+
+main : Program () Model Msg
 main =
-    Element.layout [] (text "Hello world")
+    Browser.document
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
